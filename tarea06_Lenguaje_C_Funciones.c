155b577983209ac99904bab957903e0a70076bbb
Gumaro muñoz sanchez <gumaro22@gmail.com>
2024-04-02 22:10:01 -0600

    tarea

--- tarea06_Lenguaje_C_Funciones.c
+++ tarea06_Lenguaje_C_Funciones.c
@@ -1,42 +1,40 @@
 #include <stdio.h>
 
-void suma(int num1, int num2){
-	int suma;
+void nombre_usuario(char nombre[10]){
+	printf("hola Mucho gusto...%s\n ",nombre);
+	printf("\n");
+}
+
+ void suma(float num1, float num2){
+	float suma;
 	float promedio;
 	
 	
 	
 	suma = num1 + num2;
 	printf("La suma de los numero que escribio es...\n");
-	printf ("%d + %d = %d\n",num1,num2,suma);
+	printf ("%.0f + %.0f = %.0f\n",num1,num2,suma);
 	
 	promedio = suma/2;
 	printf("El promedio de la suma  es...%.2f\n",promedio);
 	
 }
 	
-	void nombre_usuario(int nombre){
-		printf("hola%d\n",nombre);
-		printf("\n");
-	}
-
 int main(int argc, char *argv[]) {
-	int nombre;
-	int num1, num2;
-	
-	
-	printf("Por favor escriba un numero entero\n");
-	scanf ("%d", &num1);
-	
-	printf("Por favor escriba un numero entero\n");
-	scanf ("%d", &num2);
+	char nombre[10];
+	float num1, num2;
 	
 	printf("Hola podrias escribir tu nombre\n");
-	scanf("%d",&nombre);
+	scanf ("%s", &nombre);
 	
-
-	suma(num1,num2);
+	printf("Por favor escriba un numero entero\n");
+	scanf ("%f", &num1);
+	
+	printf("Por favor escriba otro un numero entero\n");
+	scanf ("%f", &num2);
+	
 	nombre_usuario(nombre);
+	suma(num1,num2);
 	
 	return 0;
 }
